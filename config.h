c2f496fde626e8844eaa8163883b9ddcb21962da
Laughing-q <1185102784@qq.com>
2021-09-29 10:25:45 +0800

    reset

--- config.h
+++ config.h
@@ -186,7 +186,7 @@ static Key keys[] = {
 	{ MODKEY,			XK_f,		togglefullscr,	{0} },
 
   /* Programs */
-	{ MODKEY,			XK_b,		spawn,		SHCMD("google-chrome") },
+	{ MODKEY,			XK_b,		spawn,		SHCMD("$BROWSER") },
 	{ MODKEY|ShiftMask,		XK_w,		spawn,		SHCMD(TERMINAL " -e sudo nmtui") },
 	/* { MODKEY,			XK_e,		spawn,		SHCMD(TERMINAL " -e neomutt ; pkill -RTMIN+12 dwmblocks; rmdir ~/.abook") }, */
 	/* { MODKEY|ShiftMask,		XK_e,		spawn,		SHCMD(TERMINAL " -e abook -C ~/.config/abook/abookrc --datafile ~/.config/abook/addressbook") }, */
