e84375bd69c63e9fd22c1668ee56b3be1b8309b8
Laughing-q <1185102784@qq.com>
2022-09-14 15:30:57 +0800

    update swallow

--- config.h
+++ config.h
@@ -61,7 +61,10 @@ static const Rule rules[] = {
 	/* class    instance      title       	 tags mask    isfloating   isterminal  noswallow  monitor */
 	{ "Gimp",     NULL,       NULL,       	    1 << 8,       0,           0,         0,        -1 },
 	{ TERMCLASS,  NULL,       NULL,       	    0,            0,           1,         0,        -1 },
+  { "ToDesk",   NULL,       NULL,             0,            1,           0,         1,        -1 },
 	{ NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
+  //  this is my personal setting when running python
+	{ NULL,       NULL,       "cv2",              0,            0,           0,         1,        -1 },
 	{ NULL,      "spterm",    NULL,       	    SPTAG(0),     1,           1,         0,        -1 },
 	{ NULL,      "spcalc",    NULL,       	    SPTAG(1),     1,           1,         0,        -1 },
 };
